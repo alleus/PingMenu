47415a6e1c4eae8d8368ea2715f410e4e30ff692
Martin Alleus <martin@appcorn.se>
2015-02-09 17:21:41 +0100

    Adding cocoapods and libraries for XML parsing

---
+++ Podfile
@@ -0,0 +1,6 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :osx, '10.10'
+
+pod 'Mantle', '1.5.4'
+pod 'KissXML', '5.0'
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// KissXML
+#define COCOAPODS_POD_AVAILABLE_KissXML
+#define COCOAPODS_VERSION_MAJOR_KissXML 5
+#define COCOAPODS_VERSION_MINOR_KissXML 0
+#define COCOAPODS_VERSION_PATCH_KissXML 0
+
+// Mantle
+#define COCOAPODS_POD_AVAILABLE_Mantle
+#define COCOAPODS_VERSION_MAJOR_Mantle 1
+#define COCOAPODS_VERSION_MINOR_Mantle 5
+#define COCOAPODS_VERSION_PATCH_Mantle 4
+
+// Mantle/extobjc
+#define COCOAPODS_POD_AVAILABLE_Mantle_extobjc
+#define COCOAPODS_VERSION_MAJOR_Mantle_extobjc 1
+#define COCOAPODS_VERSION_MINOR_Mantle_extobjc 5
+#define COCOAPODS_VERSION_PATCH_Mantle_extobjc 4
+
